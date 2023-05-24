3274b6971fccaf641d6a8f7a488285842c90077d
Kyu Lee <kwooler@gmail.com>
2023-05-23 20:05:26 -0700

    Lab 4-6

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    iternum = 0
+    N = int(input('Enter the value for N: '))
+    for i in range(N):
+        for j in """ COMPLETE FOR LOOP STATEMENT """
+            print(f'({i}, {j})', end=' ')
+            iternum += 1
+        print()
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return iternum
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,26 +10,20 @@ def test_main_1():
     datastr = '3'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == 6
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*0[\w,\W]*0[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*1[\w,\W]*0[\w,\W]*[\w,\W]*1[\w,\W]*1[\w,\W]*'
-    res = re.search(regex_string, lines[1])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*2[\w,\W]*0[\w,\W]*[\w,\W]*2[\w,\W]*1[\w,\W]*[\w,\W]*2[\w,\W]*2[\w,\W]*'
-    res = re.search(regex_string, lines[2])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*0[\w,\W]*0[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
@@ -38,31 +32,17 @@ def test_main_2():
     datastr = '5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == 15
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*0[\w,\W]*0[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*1[\w,\W]*0[\w,\W]*[\w,\W]*1[\w,\W]*1[\w,\W]*'
-    res = re.search(regex_string, lines[1])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*2[\w,\W]*0[\w,\W]*[\w,\W]*2[\w,\W]*1[\w,\W]*[\w,\W]*2[\w,\W]*2[\w,\W]*'
-    res = re.search(regex_string, lines[2])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*3[\w,\W]*0[\w,\W]*[\w,\W]*3[\w,\W]*1[\w,\W]*[\w,\W]*3[\w,\W]*2[\w,\W]*[\w,\W]*3[\w,\W]*[\w,\W]*3[\w,\W]*'
-    res = re.search(regex_string, lines[3])
-    assert res != None
-    print(res.group())
-    regex_string = r'[\w,\W]*4[\w,\W]*0[\w,\W]*[\w,\W]*4[\w,\W]*1[\w,\W]*[\w,\W]*4[\w,\W]*2[\w,\W]*[\w,\W]*4[\w,\W]*[\w,\W]*3[\w,\W]*[\w,\W]*4[\w,\W]*[\w,\W]*4[\w,\W]*'
-    res = re.search(regex_string, lines[4])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*0[\w,\W]*0[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
