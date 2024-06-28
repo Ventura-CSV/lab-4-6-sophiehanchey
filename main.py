4fdec748f1ac33feecb8403691d12f1c7a7cff09
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 13:29:28 -0700

    initial code, int not iterable

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ def main():
     iternum = 0
     N = int(input('Enter the value for N: '))
     for i in range(N):
-        for j in """ COMPLETE FOR LOOP STATEMENT """
+        for j in range(size(i)):
             print(f'({i}, {j})', end=' ')
             iternum += 1
         print()
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return iternum
 
 
