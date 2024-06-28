a220093a991da083b0ce6a93de79f9197f67792b
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 13:30:12 -0700

    range(i)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     iternum = 0
     N = int(input('Enter the value for N: '))
     for i in range(N):
-        for j in range(size(i)):
+        for j in range(i):
             print(f'({i}, {j})', end=' ')
             iternum += 1
         print()
